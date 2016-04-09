a093100a014874fb78c20b907df9e472390e69c9
David Reyes <david.proteco@gmail.com>
2016-04-09 18:54:29 -0500

    Change gpio pins to 29 31 33

--- bash/runRGB.sh
+++ bash/runRGB.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #llama los GPIO a usar
-$(bash GPIO1.sh 5 2>> error3.log || salir) &
-$(bash GPIO2.sh 6 2>> error3.log || salir) &
-$(bash GPIO3.sh 13 2>> error3.log || salir) &
+$(bash GPIO1.sh 29 2>> error3.log || salir) &
+$(bash GPIO2.sh 31 2>> error3.log || salir) &
+$(bash GPIO3.sh 33 2>> error3.log || salir) &
